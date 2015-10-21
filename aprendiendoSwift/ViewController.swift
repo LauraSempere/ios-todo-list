f9287ba7efb7703ce750d82707b4a29615484a7d
Laura Sempere <lrsempere@gmail.com>
2015-10-21 21:51:39 +0800

    Initial commit

---
+++ aprendiendoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aprendiendoSwift
+//
+//  Created by Laura Scully on 21/10/15.
+//  Copyright © 2015 laura.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
