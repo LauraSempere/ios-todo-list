f4854a08d6b3b82b1090ca2105b4ca7146765b17
Laura Sempere <lrsempere@gmail.com>
2015-10-23 19:27:37 +0800

    Guardar tareas en disco

--- aprendiendoSwift/TodoList.swift
+++ aprendiendoSwift/TodoList.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class TodoList: NSObject {
     var items: [String] = []
+    
+    override init() {
+        super.init()
+        loadItems()
+    }
+    
+    private let fileURL: NSURL = {
+        let fileManager = NSFileManager.defaultManager()
+        let documentDirectoryURLs = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+        let documentDirectoryURL = documentDirectoryURLs.first!
+        print("path de documents: \(documentDirectoryURL)")
+        return documentDirectoryURL.URLByAppendingPathComponent("todolist.items")
+    }()
+    
     func addItem (item: String){
         items.append(item)
+        saveItems()
     }
+    
+    
+    func saveItems(){
+        let itemsArray = items as NSArray
+        if itemsArray.writeToURL(self.fileURL, atomically: true){
+            print("guardado")
+        }else{
+            print("no se pudo guardar")
+        }
+    }
+    
+    func loadItems(){
+        if let itemsArray = NSArray(contentsOfURL: self.fileURL) as? [String]{
+            self.items = itemsArray
+        }
+    }
+    
 }
 
 extension TodoList : UITableViewDataSource {
