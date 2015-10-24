f9d20f8959d7cbacb3767bcf616b70d496ae5dcc
Laura Sempere <lrsempere@gmail.com>
2015-10-24 13:18:32 +0800

    Remove item from the list & Hide keyboard

--- aprendiendoSwift/ViewController.swift
+++ aprendiendoSwift/ViewController.swift
@@ -8,26 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var itemTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     let todoList = TodoList()
     
     @IBAction func addButtonPressed (sender: UIButton) {
+        if (itemTextField.text != "") {
         print("Agregando un elemento a la lista \(itemTextField.text)")
         todoList.addItem(itemTextField.text!)
-        tableView.reloadData()
+            tableView.reloadData()}
+        self.itemTextField?.resignFirstResponder()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         tableView.dataSource = todoList
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    //MARK: Methods about the Table
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        self.itemTextField?.resignFirstResponder()
     }
 
 
